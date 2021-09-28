950e5aa840a91e34cbbbc4be5a3f7df6444d5ccd
root <kendal.bozkurt@bulutfon.com>
2021-09-28 10:49:51 +0300

    tests created

--- test/controllers/answers_controller_test.rb
+++ test/controllers/answers_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class AnswersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get answers_url
+    assert_response :success
+    assert_equal "index", @controller.action_name
+    assert_match "Answers", @response.body
+  end
+
+  test "should create answers" do
+    post answers_url, params: {survey: {answers: {"answer-1": {"question_id": "1", "answer_text": "answer 1"}}}}
+    assert_redirected_to answers_path
+  end
 end
