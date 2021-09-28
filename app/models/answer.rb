63497e2a0e558862cf9cfb0d4258a70b4fbe72ee
root <kendal.bozkurt@bulutfon.com>
2021-09-28 10:14:06 +0300

    validate answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,8 @@
 class Answer < ApplicationRecord
   belongs_to :question, required: true
   belongs_to :question_option, required: false
+
+  validates :question_option_id, presence: true, unless: :answer_text
+  validates :answer_text, presence: true, unless: :question_option_id
 end
 
